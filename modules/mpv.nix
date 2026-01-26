14bb37f9c352907a5573f60f64ca2f8c1b67ea71
Sagar Mishra <sagarmish1234@gmail.com>
2026-01-26 15:30:15 +0530

    Mpv config

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -1,4 +1,35 @@
-{ ... }:
-{
-  programs.mpv.enable = true;
+{pkgs, ...}: {
+  programs.mpv = {
+    enable = true;
+    bindings = {
+      "UP" = "add volume 5"; # Increase volume by 5%
+      "DOWN" = "add volume -5"; # Decrease volume by 5%
+    };
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          # uosc
+          sponsorblock
+          modernz
+          autosub
+          quality-menu
+          autosubsync-mpv
+          mpv-notify-send
+          thumbfast
+          mpv-playlistmanager
+          memo
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+    );
+
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+    };
+  };
 }
