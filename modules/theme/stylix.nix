9823f8842575e26e3d30e34daff328bb17cc551e
Sagar Mishra <sagarmish1234@gmail.com>
2026-03-14 14:19:02 +0530

    nix update and stylix theme change

--- modules/theme/stylix.nix
+++ modules/theme/stylix.nix
@@ -1,10 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     polarity = "dark";
   };
   stylix.targets.gtk.enable = false;
+  stylix.targets.emacs.enable = false;
   stylix.targets.noctalia-shell.enable = false;
   gtk = {
     enable = true;
