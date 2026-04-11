4a72ebaa715f714fba8d57cf790a81c922800748
Sagar Mishra <sagarmish1234@gmail.com>
2026-04-11 16:31:59 +0530

    add plymouth config

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,31 @@
     # kernelPackages = pkgs.linuxKernel.packagesFor pkgs.cachyosKernels.linux-cachyos-latest;
     # Kernel
     kernelPackages = pkgs.linuxPackages_testing;
+
+    plymouth = {
+      enable = true;
+      theme = "green_blocks";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "green_blocks" ];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+
   };
 
   # Set your time zone.
