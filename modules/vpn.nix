81b2702a5ca235a80dc171cc4adc9dcac33b5218
Sagar Mishra <sagarmish1234@gmail.com>
2026-05-16 20:30:31 +0530

    fix: remove misplaced vpn module

--- modules/vpn.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  # Enable the Cloudflare WARP daemon
-  services.cloudflare-warp.enable = true;
-
-  # Add the CLI tool to system packages
-  environment.systemPackages = [ pkgs.cloudflare-warp ];
-}
