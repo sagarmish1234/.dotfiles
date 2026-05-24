44d0f56e506068ac10ebcee9b2a36019e7bf38fb
Sagar Mishra <sagarmish1234@gmail.com>
2026-05-24 12:11:50 +0530

    Add uBlock Origin, SponsorBlock, ClearURLs, and Return YouTube Dislike extensions

--- modules/browsers/zen-browser.nix
+++ modules/browsers/zen-browser.nix
@@ -13,6 +13,22 @@ in
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
           installation_mode = "force_installed";
         };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "sponsorBlocker@ajay.app" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{7493aaa9-f9ba-4705-9e60-f421f1d1844b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{762f9885-ad34-4054-9467-3367184478b0}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislike/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
     profiles.default = {
