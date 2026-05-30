e3b18861de5d1a344dee375bdd39c26ef02a94b0
Sagar Mishra <sagarmish1234@gmail.com>
2026-05-30 21:35:59 +0530

    Boot speedup first commit

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -12,13 +12,22 @@
 
   # Host-specific settings
   networking.hostName = "nixos";
+  networking.networkmanager.wifi.powersave = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.timeout = 1;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use XanMod kernel for better responsiveness.
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
+  # Modern initrd with systemd
+  boot.initrd.systemd.enable = true;
+  boot.initrd.verbose = false;
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 
   # Performance-oriented kernel parameters
   boot.kernelParams = [
@@ -26,6 +35,13 @@
     "splash"
     "pcie_aspm=performance"
     "nvme_load=1"
+    "fastboot"
+    "nowatchdog"
+    "nvidia-drm.modeset=1"
+    "8250.nr_uarts=1"
+    "tpm_tis.interrupts=0"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
   ];
 
   # This value determines the NixOS release from which the default
